c65df857586e8536f06e4cbb445e4abe4ff7b972
DavyJoness <matikielpinski@gmail.com>
2019-03-03 22:30:19 +0100

    Przygotowane głowne menu

---
+++ NaukaSlowek/ExecuteTest.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NaukaSlowek
+{
+    /// <summary>
+    /// Interaction logic for ExecuteTest.xaml
+    /// </summary>
+    public partial class ExecuteTest : Window
+    {
+        public ExecuteTest()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            MainWindow.mw.Visibility = Visibility.Visible;
+        }
+    }
+}
--- NaukaSlowek/MainWindow.xaml.cs
+++ NaukaSlowek/MainWindow.xaml.cs
@@ -20,9 +20,82 @@ namespace NaukaSlowek
     /// </summary>
     public partial class MainWindow : Window
     {
+        public enum Lang
+        {
+            English,
+            French,
+            Latin
+        }
+
+        public enum Option
+        {
+            CreateTest,
+            MakeTest
+        }
+
+        public Lang lang;
+        public Option option;
+        public static MainWindow mw;
+
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)e.Source;
+            switch (b.Name)
+            {
+                case "ButtonEng":
+                    lang = Lang.English;
+                    LabelLangSelected.Content = "Wybrano angielski";
+                    break;
+                case "ButtonFre":
+                    lang = Lang.French;
+                    LabelLangSelected.Content = "Wybrano francuski";
+                    break;
+                case "ButtonLat":
+                    lang = Lang.Latin;
+                    LabelLangSelected.Content = "Wybrano łaciński";
+                    break;
+            }
+
+            showOptions();
+        }
+
+        private void showOptions()
+        {
+            StackLangs.Visibility = Visibility.Hidden;
+            StackOptions.Visibility = Visibility.Visible;
+        }
+
+        private void showLangs()
+        {
+            StackLangs.Visibility = Visibility.Visible;
+            StackOptions.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Make(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+            ExecuteTest et = new ExecuteTest();
+            mw = this;
+            et.Show();
+        }
+
+        private void Button_Create(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+            CreateTest et = new CreateTest();
+            mw = this;
+            et.Show();
+        }
+
+        private void Button_Back(object sender, RoutedEventArgs e)
+        {
+            showLangs();
         }
     }
 }
